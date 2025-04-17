fix stats computation

scripts/compute_stats_forward_all.py
@@ -1,7 +1,7 @@
 import torch
 import numpy as np
-from utils import compute_all_stats, get_names_of_image_datasets
 
+from utils import compute_all_stats, get_names_of_image_datasets
 from config import Config
 from utils import get_data_tensor, with_config
 
@@ -10,10 +10,13 @@ from utils import get_data_tensor, with_config
 def main(config: Config) -> None:
     config.forward_stats.unbiased = False
     for dataset_name in get_names_of_image_datasets():
+        print(dataset_name)
         config.data.dataset_name = dataset_name
         data = get_data_tensor(config)
-        min_temp, max_temp = config.diffusion.temp_range
+        min_temp, max_temp = config.forward_stats_temp_range
         temp = torch.logspace(np.log10(min_temp), np.log10(max_temp), config.forward_stats.n_temps)
+        if config.diffusion.min_temp < temp[-1]:
+            temp = torch.cat((torch.full((1,), config.diffusion.min_temp), temp))
         fwd_stats_cfg = config.forward_stats
         stats = compute_all_stats(data, temp, fwd_stats_cfg.n_samples, fwd_stats_cfg.batch_size)
         np.savez(config.forward_stats_path, **stats) # type: ignore

utils/data.py
@@ -27,9 +27,9 @@ class ImageDataset(Dataset[tuple[Tensor, ...]]):
     DATASET_CLASSES = {
         "mnist": MNIST,
         "cifar10": CIFAR10,
-        "cifar100": CIFAR100,
-        "fashion_mnist": FashionMNIST,
-        "image_net": ImageNet,
+        # "cifar100": CIFAR100,
+        # "fashion_mnist": FashionMNIST,
+        # "image_net": ImageNet,
         "celeba": CelebA,
     }
 
@@ -40,7 +40,6 @@ class ImageDataset(Dataset[tuple[Tensor, ...]]):
         transform = Compose([
             Resize(image_size[1:]),
             ToTensor(),
-            # Lambda(lambda x: x.half()),
             *(() if dataset_name == "mnist" else (Normalize(mean=0.5, std=0.5),)),
         ])
         self.dataset = self.DATASET_CLASSES[dataset_name](
@@ -93,7 +92,7 @@ def get_data_generator(
 def get_data_tensor(config: Config, train: bool = True) -> Tensor:
     dataset = get_dataset(config, train=train)
     dataloader = DataLoader(dataset, batch_size=500, shuffle=False, num_workers=8)
-    return torch.cat([x[0] for x in tqdm(dataloader)])
+    return torch.cat([x[0] for x in tqdm(dataloader, desc="Loading data ...")])
 
 
 def to_uint8(images: Tensor, values_range: tuple[float, float] = (-1, 1)) -> Tensor:

utils/stats.py
@@ -59,7 +59,7 @@ def compute_stats(
     _x0 = x0
     temp = temp.to(device)
     batch_stats: dict[str, list[Tensor]] = defaultdict(list)
-    for _ in trange(ceil(n_samples / batch_size), desc="Computing statistics..."):
+    for _ in trange(ceil(n_samples / batch_size), desc=f"Computing {'unbiased ' if unbiased else ''}statistics..."):
         if unbiased:
             idx = np.random.choice(range(len(x0)))
             traj_start = x0[torch.full((batch_size, 1), idx).long()]