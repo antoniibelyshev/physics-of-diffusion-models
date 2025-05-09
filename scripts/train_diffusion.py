a0baa769644bb959ba185ee9973499290085755f
Antonii Belyshev <antonii.belyshev@gmail.com>
2025-05-09 13:36:30 +0000

    continue experiments training ddpm

--- scripts/train_diffusion.py
+++ scripts/train_diffusion.py
@@ -7,6 +7,8 @@ from utils import with_config
 
 @with_config(parse_args=(__name__ == "__main__"))
 def main(config: Config) -> None:
+    torch._dynamo.config.cache_size_limit = 64
+
     dataset = get_dataset(config)
     data_generator = get_data_generator(dataset, config.ddpm_training.batch_size)
     ddpm = DDPM.from_config(config)
